7a20bc63d97cd4ae219977e4271b5c7bf0a40868
thomashedrich <ehedrich@udesa.edu.ar>
2025-06-05 14:44:54 -0300

    Borrados los archivos de generos, cree uno general para luego a traves de un .js llenarlo de peliculas de sus respectivos generos

---
+++ js/generos-series.js
@@ -0,0 +1,24 @@
+let API_KEY = 'dd7272a10fa82bffe9a6d08d4f2cff02';
+let URL = 'https://api.themoviedb.org/3/genre/tv/list?api_key=' + API_KEY + '&language=es-ES';
+
+let lista = document.querySelector('.lista-generos');
+
+fetch(URL)
+  .then(function(respuesta) {
+    return respuesta.json();
+  })
+  .then(function(data) {
+    let generos = data.genres;
+
+    for (let i = 0; i < generos.length; i++) {
+      let genero = generos[i];
+      lista.innerHTML += `
+        <li>
+          <a href="detail-genre.html?id=${genero.id}&tipo=serie">${genero.name}</a>
+        </li>   
+      `;
+    }
+  })
+  .catch(function(error) {
+    console.log("Error al cargar géneros de series: ", error);
+  });
